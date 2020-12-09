abd361718c3dd8785f008e231c5dc9e5b692cfb5
hikaru yamashita <10268578+karunru@users.noreply.github.com>
2020-12-09 13:01:53 +0900

    add groupby developer

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -2,3 +2,4 @@ from .base import generate_features, load_features
 from .basic import Basic
 from .groupby_name import GroupbyName
 from .groupby_publisher import GroupbyPublisher
+from .groupby_developer import GroupbyDeveloper
